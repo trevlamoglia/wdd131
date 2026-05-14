fcc31852412e64fe00c1a85739086ddfc777156a
trevlamoglia <trevor.lamoglia34@gmail.com>
2026-05-14 13:19:54 -0600

    Cool pics pt.2

---
+++ coolpics/script.js
@@ -0,0 +1,42 @@
+// Dropdown Menu
+const menuButton = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('nav');
+
+menuButton.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    menuButton.classList.toggle('change');
+});
+
+
+// Modals
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    const img = e.target.closest('img');
+    if (!img) return;
+
+// console.log(e.target);
+
+    modalImage.src = img.src;
+    modalImage.alt = img.alt;
+
+    modal.showModal();
+}
+
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
